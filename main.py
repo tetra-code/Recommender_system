37cac03491ac814095370f9d13e5e171d294b9d0
Philip <>
2023-01-11 12:26:31 +0100

    latent factor implementation

--- main.py
+++ main.py
@@ -21,8 +21,42 @@ def find_biases(
     return movie_bias, user_bias
 
 
-def find_latent_factors():
-    return -2
+"""
+To ask:
+
+does ratings data have to be separated to train and test when runing latent factors
+
+"""
+def find_latent_factors(
+        num_users: int,
+        num_user_factors: int,
+        num_movies: int,
+        num_movie_factors: int,
+        alpha: float,
+        epochs: int,
+        ratings_data: np.ndarray,
+) -> np.ndarray:
+    # P is user factor matrix (later needs to be transformed when multiplying)
+    P = np.random.normal(0, .1, (num_users, num_user_factors))
+
+    # Q is movie factor matrix
+    Q = np.random.normal(0, .1, (num_movies, num_movie_factors))
+
+    for epoch in range(epochs):
+        for row in ratings_data:
+            x = int(row[0])
+            i = int(row[1])
+            r_xi = float(row[2])
+            residual = r_xi - np.dot(P[x], Q[i])
+
+            # Stochastic gradient
+            # we want to update them at the same time, so we make a temporary variable.
+            temp = P[x, :]
+            P[x, :] += alpha * residual * Q[i]
+            Q[i, :] += alpha * residual * temp
+
+    ratings_matrix = P.T * Q
+    return ratings_matrix
 
 
 def main():
@@ -34,4 +68,12 @@ def main():
 
     # 3.58131
     global_avg_rating = utility.all_movies_average_rating(ratings_data)
-
+    ratings_matrix = find_latent_factors(
+        num_users=6040,
+        num_user_factors=3,
+        num_movies=3706,
+        num_movie_factors=2,
+        alpha=0.3,
+        epochs=300,
+        ratings_data=ratings_data,
+    )
